9650bff1039ab85629f8f91079b36f18327b6d18
Osama Nadeem <osama.nadeem@consoliads.com>
2022-08-22 11:53:12 +0500

    update config and version.

---
+++ Example/build/Pods.build/Debug-iphoneos/ConsoliAds-Mopub-Adapter.build/DerivedSources/ConsoliAds_Mopub_Adapter_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ConsoliAds_Mopub_AdapterVersionString[];
+ extern const double ConsoliAds_Mopub_AdapterVersionNumber;
+
+ const unsigned char ConsoliAds_Mopub_AdapterVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ConsoliAds_Mopub_Adapter  PROJECT:Pods-1" "\n";
+ const double ConsoliAds_Mopub_AdapterVersionNumber __attribute__ ((used)) = (double)1.;
